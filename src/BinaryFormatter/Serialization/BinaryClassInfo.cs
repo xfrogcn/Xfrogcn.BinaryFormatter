60fc44047118b74b3dd609c8e1d164911d228d47
xfrogcn <xfrogcn@163.com>
2020-11-30 21:27:40 +0800

    BinaryClassInfo

---
+++ src/BinaryFormatter/Serialization/BinaryClassInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Xfrogcn.BinaryFormatter
+{
+    [DebuggerDisplay("ClassType.{ClassType}, {Type.Name}")]
+    internal sealed partial class BinaryClassInfo
+    {
+    }
+}
