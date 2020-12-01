440bcfdc6523b015c6160d2e8a6b7a6540739681
xfrogcn <xfrogcn@163.com>
2020-12-01 21:42:23 +0800

    BinaryConverter

---
+++ src/BinaryFormatter/Serialization/ReadStack.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace Xfrogcn.BinaryFormatter
+{
+    internal struct ReadStack
+    {
+    }
+}
--- src/BinaryFormatter/Serialization/WriteStack.cs
+++ src/BinaryFormatter/Serialization/WriteStack.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Xfrogcn.BinaryFormatter
+﻿namespace Xfrogcn.BinaryFormatter
 {
     internal struct WriteStack
     {
