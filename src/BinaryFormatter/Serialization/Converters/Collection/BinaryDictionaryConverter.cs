be4924cabe9f02508ce4fdb39a9ff49d483d74db
xfrogcn <xfrogcn@163.com>
2020-12-22 17:11:38 +0800

    BinaryDictionaryConverter

---
+++ src/BinaryFormatter/Serialization/Converters/Collection/BinaryDictionaryConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xfrogcn.BinaryFormatter.Serialization.Converters
+{
+    internal abstract class BinaryDictionaryConverter<T> : BinaryResumableConverter<T>
+    {
+        internal sealed override ClassType ClassType => ClassType.Dictionary;
+        protected internal abstract bool OnWriteResume(BinaryWriter writer, T dictionary, BinarySerializerOptions options, ref WriteStack state);
+    }
+}
